1d232b8b42cbc80ccbb67a67a6bfb172187da7cd
ako <a.veysinejad@gmail.com>
2022-03-03 23:12:05 +0330

    Changes: - Changed config files to generate reduce size of the package. - Changed rollup config to add banner to the generated files.

--- rollup.config.js
+++ rollup.config.js
@@ -1,32 +1,46 @@
 import resolve from "@rollup/plugin-node-resolve";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
-const packageJson = require("./package.json");
+const pkg = require("./package.json");
+
+const inputFileName = "src/index.ts";
+const moduleName = pkg.name.replace(/^@.*\//, "");
+const author = pkg.author;
+const banner = `
+  /**
+   * @license
+   * author: ${author}
+   * ${moduleName}.js v${pkg.version}
+   * Released under the ${pkg.license} license.
+   */
+`;
 
 export default [
   {
-    input: "src/index.ts",
+    input: inputFileName,
     output: [
       {
-        file: packageJson.main,
+        file: pkg.main,
         format: "cjs",
-        sourcemap: true
+        sourcemap: "true",
+        banner
       },
       {
-        file: packageJson.module,
+        file: pkg.module,
         format: "esm",
-        sourcemap: true
+        sourcemap: "true",
+        banner
       }
     ],
     plugins: [peerDepsExternal(), resolve(), commonjs(), typescript({ tsconfig: "./tsconfig.build.json" }), postcss(), terser()]
   },
   {
-    input: "dist/esm/types/index.d.ts",
+    input: "dist/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
     external: [/\.css$/]
